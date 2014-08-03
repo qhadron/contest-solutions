8fb8e96af1ea7d4eb8d036f0c51c4a1ceb1aafea
Jack Li <jack.lee2980@gmail.com>
2014-08-03 19:36:56 -0400

    Added a sudoku solver
    Signed-off-by: Jack Li <jack.lee2980@gmail.com>

---
+++ sudoku.cpp
@@ -0,0 +1,332 @@
+#include "sudoku.h"
+
+using namespace std;
+
+Cell::Cell(int X, int Y, int Value) : x(X), y(Y), value(Value) {
+	if (!Value) {
+		memset(possibleValues, true, sizeof(possibleValues));
+		count();
+	}
+	else {
+		possibilities = 0;
+		memset(possibleValues, 0, sizeof(possibleValues));
+	}
+};
+void Cell::notify() {
+	for (Test* cur : tests) {
+		cur->check();
+	}
+}
+bool Cell::operator<(const Cell& rhs) {
+	return possibleValues < rhs.possibleValues;
+}
+void Cell::count() {
+	possibilities = 0;
+	int tempvalue;
+	for (int i = 1; i <= 9; ++i)
+		if (possibleValues[i]) {
+			++possibilities;
+			tempvalue = i;
+		}
+	if (possibilities == 1) {
+		value = tempvalue;
+	}
+}
+
+void cellcount(Cell*& c) {
+	c->count();
+}
+
+Test::Test(string Name) : name(Name) {
+	cnt = 0;
+	for (int i = 0; i < TEST_SIZE; ++i)
+		cells[i] = nullptr;
+	memset(missing, true, sizeof(missing));
+};
+
+void SimpleTest::check() {
+	for (Cell* cur : cells) {
+		if (cur != nullptr) {
+			cur->count();
+#ifdef _DEBUG
+#endif
+			if (cur->value || cur->possibilities < 1) {
+				missing[cur->value] = false;
+				cnt++;
+			}
+		}
+	}
+	for (Cell* cur : cells) {
+		if (cur != nullptr) {
+			for (int i = 1; i <= 9; ++i)
+				if (!missing[i]) {
+					cur->possibleValues[i] = false;
+				}
+			cur->count();
+		}
+	}
+}
+
+bool Test::operator<(const Test& rhs) {
+	return cnt < rhs.cnt;
+}
+
+ostream& operator<<(ostream& os, const Cell& cell) {
+	os << "Cell@(" << cell.x << "," << cell.y << "):" << endl;;
+	for (int i = 0; i < cell.possibilities; ++i) {
+		if (cell.possibleValues[i]) {
+			os << (i + 1) << " ";
+		}
+	}
+	return os;
+}
+
+ostream& operator<<(ostream& os, const Test& test) {
+	os << "Test@" << test.name << ":" << endl;
+	for (int i = 0; i < TEST_SIZE; ++i) {
+		if (test.cells[i] != nullptr) {
+			cout << *test.cells[i] << endl;
+		}
+	}
+	return os;
+}
+
+void print() {
+	for (int y = 0; y < BOARD_Y; ++y) {
+		if (y % 3 == 0) {
+			for (int i = 0; i < BOARD_X; ++i)
+				printf("--");
+			printf("\n");
+		}
+		for (int x = 0; x < BOARD_X; ++x) {
+			cout << ((board[x][y]->value) ? (char) (board[x][y]->value + '0') : '?')
+				<< (char) (((x + 1) % 3 == 0) ? '|' : ' ');
+		}
+		printf("\n");
+	}
+	for (int i = 0; i < BOARD_X; ++i)
+		printf("--");
+	printf("\n");
+}
+
+void printpossible() {
+	for (int y = 0; y < BOARD_Y; ++y) {
+		if (y % 3 == 0) {
+			for (int i = 0; i < BOARD_X; ++i)
+				printf("--");
+			printf("\n");
+		}
+		for (int x = 0; x < BOARD_X; ++x) {
+			cout << (char) (board[x][y]->possibilities + '0')
+				<< (char) (((x + 1) % 3 == 0) ? '|' : ' ');
+		}
+		printf("\n");
+	}
+	for (int i = 0; i < BOARD_X; ++i)
+		printf("--");
+	printf("\n");
+}
+
+void exit() {
+	for (int y = 0; y < BOARD_Y; ++y) {
+		for (int x = 0; x < BOARD_X; ++x) {
+			if (board[x][y] != nullptr) {
+				delete board[x][y];
+				board[x][y] = nullptr;
+			}
+		}
+	}
+	tests.clear();
+	while (!pqueue.empty()) {
+		auto T = pqueue.top();
+		pqueue.pop();
+		if (T != nullptr)
+			delete T;
+	}
+}
+
+void checkall() {
+	for (auto it = tests.begin(); it != tests.end(); ++it) {
+		(*it)->check();
+	}
+	pqueue.update(cellcount);
+}
+
+bool dfs(Cell* cur) {
+	if (pqueue.empty())
+		return true;
+	if (cur->value) {
+		dprintf("[%d,%d] is %d\n", cur->x, cur->y, cur->value);
+		//chose next best cell from priority_queue
+		Cell* temp = pqueue.top();
+		pqueue.pop();
+		if (dfs(temp))
+			return true;
+		else {
+			//remove side-effects
+			if (temp->isGuess)
+				pqueue.push(temp);
+			return false;
+		}
+	}
+	dprintf("At [%d,%d]\n", cur->x, cur->y);
+	for (int i = 1,use; i <= 9; ++i) {
+		if (cur->possibleValues[i]) {
+			dprintf("Trying [%d,%d] from [%d,%d]\n");
+			use = true;
+			//row
+			for (int x = 0; x < 9; ++x)
+				if (board[x][cur->y]->value == i) {
+					dprintf("\tFailed: Row test at [%d,%d]\n", x, cur->y);
+					use=false;
+					break;
+				}
+			if (!use)
+				continue;
+			//column
+			for (int y = 0; y < 9; ++y)
+				if (board[cur->x][y]->value == i) {
+					dprintf("\tFailed: Col test at [%d,%d]\n", cur->x, y);
+					use = false;
+					break;
+				}
+			if (!use)
+				continue;
+			//grid
+			for (int x = cur->x / 3, dx = 0; dx < 3; ++dx) {
+				for (int y = cur->y / 3, dy = 0;use && dy < 3; ++dy) {
+					if (board[x][y]->value == i) {
+						dprintf("\tFailed: Grid test at [%d,%d]\n", x, y);
+						use = false;
+						break;
+					}
+				}
+			}
+			if (!use)
+				continue;
+			dprintf("\tSetting value of [%d,%d] to %d\n", cur->x, cur->y, cur->value);
+			cur->possibilities = 1;
+			cur->value = i;
+			checkall();
+			//chose next best cell from priority_queue
+			Cell* temp = pqueue.top();
+			pqueue.pop();
+			if (dfs(temp))
+				return true;
+			else {
+				//reinsert node upon failure
+				pqueue.push(temp);
+			}
+		}
+	}
+}
+
+Cell::~Cell() {
+	this->tests.clear();
+}
+
+int main() {
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+#endif
+#if _VERBOSE
+	freopen("output.txt", "w", stdout);
+#endif
+	while (cin) {
+		puts("--------------------------------------------------------------------------------");
+		for (int y = 0, temp; y < BOARD_Y; ++y) {
+			for (int x = 0; x < BOARD_X; ++x) {
+				if (!(cin >> temp)) {
+					if (y != 0 || x != 0)
+						cout << "Not enough info!" << endl;
+					else
+						cout << "Done reading" << endl;
+					exit();
+					return 0;
+				}
+				board[x][y] = new Cell(x + 1, y + 1, temp);
+			}
+		}
+		print();
+		//test each row
+		for (int i = 0; i < BOARD_Y; ++i) {
+			SimpleTest* temp = new SimpleTest((string)"Row Test " + (char) (i + 1 + '0'));
+			for (int j = 0; j < BOARD_X; ++j) {
+				temp->cells[j] = board[j][i];
+			}
+			tests.push_back(temp);
+		}
+		//test each column
+		for (int i = 0; i < BOARD_X; ++i) {
+			SimpleTest* temp = new SimpleTest((string)"Column Test " + (char) (i + 1 + '0'));
+			for (int j = 0; j < BOARD_Y; ++j) {
+				temp->cells[j] = board[i][j];
+			}
+			tests.push_back(temp);
+		}
+		//test each grid
+		for (int y = 0, cnt = 0; y < 3; ++y)
+			for (int x = 0; x < 3; ++x) {
+				SimpleTest* temp = new SimpleTest((string)"Grid test " + (char) (++cnt + '0'));
+				for (int dx = 0, i = 0; dx < 3; ++dx) {
+					for (int dy = 0; dy < 3; ++dy) {
+						temp->cells[i++] = board[x * 3 + dx][y * 3 + dy];
+					}
+				}
+				tests.push_back(temp);
+			}
+
+#if _VERBOSE
+		printf("%d tests in total:\n", tests.size());
+		for (Test* cur : tests) {
+			cout << (*cur) << endl;
+		}
+#endif
+		bool succeeded = true;
+#if _SOLVE_BY_CELL
+		for (int i = 0; i < BOARD_X; ++i)
+			for (int j = 0; j < BOARD_Y; ++j)
+				if (!board[i][j]->value)
+					pqueue.push(board[i][j]);
+		pqueue.update(cellcount);
+
+		while (!pqueue.empty()) {
+			checkall();
+#if _VERBOSE
+			printpossible();
+#endif
+			Cell* cur = pqueue.top();
+			pqueue.pop();
+			assert(cur != nullptr);
+#ifdef _DEBUG
+			printf("At (%d,%d): val:%4d pos:%4d\n", cur->x, cur->y, cur->value, cur->possibilities);
+#endif
+			if (cur->possibilities) {
+				if (cur->value != 0) {
+#ifdef _DEBUG
+					printf("The only possible number in [%d,%d] is %d.\n", cur->x, cur->y, cur->value);
+#endif
+					continue;
+				}
+				else {
+					cout << "Unexpected error" << endl;
+					succeeded = false;
+					break;
+				}
+			}
+			if (cur->possibilities > 1) {
+				cout << "Not enough info " << endl;
+				succeeded = false;
+				break;
+			}
+		}
+#else
+#endif
+		if (!succeeded) {
+			cout << "Failed. Current board: " << endl;
+		}
+		print();
+		exit();
+	}
+	return 0;
+}
---
+++ sudoku.h
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <list>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <stack>
+
+#define TEST_SIZE 10
+
+#define BOARD_X 9
+#define BOARD_Y 9
+
+#define _VERBOSE 0
+#define _SOLVE_BY_CELL 1
+
+#ifdef _DEBUG
+#define dprintf printf
+#else
+#define dprintf(x) 
+#endif
+
+#ifdef _VERBOSE
+#define vprintf printf
+#else
+#define vprintf(x) 
+#endif
+
+using namespace std;
+
+struct Test;
+struct Cell;
+
+struct Cell {
+	int x, y;
+	int value;
+	bool isGuess;
+	list<Test*> tests;
+	bool possibleValues[10];
+	int possibilities;
+	Cell(int X, int Y, int Value);
+	void notify();
+	bool operator<(const Cell& rhs);
+	void count();
+	~Cell();
+};
+
+struct Test {
+	int cnt;
+	bool missing[10];
+	string name;
+	Cell* cells[TEST_SIZE];
+	Test(string Name);
+	virtual void check(){};
+	bool operator<(const Test& rhs);
+};
+
+struct SimpleTest : Test {
+	SimpleTest(string Name) : Test(Name) {}
+	virtual void check();
+};
+
+//test based on the idea that if N cells share the same N possibilites only, then
+//if another cell also shares those possibilites, but has other options,
+//then the other cell cannot be contain one of the previous conditions
+struct SubsetTest : Test {
+	SubsetTest(string Name) : Test(Name) {}
+};
+
+bool dfs(Cell* cur);
+
+struct Comparator
+{
+#if _SOLVE_BY_CELL
+	bool operator() (const Cell* a, const Cell* b) {
+		return a->possibilities > b->possibilities;
+	}
+#else
+	bool operator() (const Test* a, const Test* b) {
+		return a->cnt > b->cnt;
+	}
+#endif
+};
+
+template <typename T, class Container, class Comp>
+class priority_queue {
+public:
+	priority_queue() {
+		make_heap(c.begin(), c.end(), Comp());
+	}
+	priority_queue(priority_queue& other) {
+		c(other.c);
+		make_heap(c.begin(), c.end(), Comp());
+	}
+	void push(T val) {
+		c.push_back(val);
+		std::push_heap(c.begin(), c.end(), Comp());
+	}
+	void pop() {
+		assert(is_heap(c.begin(), c.end(), Comp()));
+		pop_heap(c.begin(), c.end(), Comp());
+		c.pop_back();
+	}
+	T top() {
+		assert(!c.empty());
+		return (c.front());
+	}
+	void update() {
+		make_heap(c.begin(), c.end(), Comp());
+		assert(is_heap(c.begin(), c.end(), Comp()));
+#ifdef _DEBUG
+		cout << "updated" << endl;
+#endif
+	}
+	void update(void(*f)(T&)) {
+		for (auto it = c.begin(); it != c.end(); ++it)
+			f(*it);
+		update();
+	}
+	bool empty() {
+		return c.empty();
+	}
+	Container base_val(){
+		return Container(c);
+	}
+	Container& container_base() {
+		return c;
+	}
+	size_t size() {
+		return c.size();
+	}
+	void sort() {
+		sort_heap(c.begin(), c.end, Comp());
+	}
+private:
+	Container c;
+};
+
+Cell* board[BOARD_X][BOARD_Y];
+list<Test*> tests;
+#if _SOLVE_BY_CELL
+priority_queue<Cell*, vector<Cell*>, Comparator> pqueue;
+#else
+priority_queue<Test*, vector<Test*>, Comparator> pqueue;
+#endif
