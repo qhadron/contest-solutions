927ff806a6d238342096ddfa48637839c839200e
Jack Li <jack.lee2980@gmail.com>
2014-12-04 21:59:19 -0500

    Changed test.cpp to caluclate pi instead

--- test.cpp
+++ test.cpp
@@ -1,26 +1,38 @@
 #include <cstdio>
+#include <iostream>
+#include <ctime>
+#include <fstream>
+#include <cmath>
+#include <thread>
+
 
 using namespace std;
 
 char _;
 
-#define scan(x) while((x=getchar())<'0'); for (x-='0';(_=getchar())>='0';x=(x<<1)+(x<<3)+_-'0');
+
+#define scan(x) do {while((x=getchar())<'0'); for (x-='0';(_=getchar())>='0';x=(x<<1)+(x<<3)+_-'0');}while(0)
 
 template <typename T>
-void readInt(T& x) {
-    while ((x=getchar())<'0');
-    for (x-='0';(_=getchar())>='0';)
-        x =(x<<3)+(x<<1) + _-'0';
+inline void readInt(T& x) {
+	while ((x = getchar()) < '0');
+	for (x -= '0'; (_ = getchar()) >= '0';)
+		x = (x << 3) + (x << 1) + _ - '0';
 }
 
+#define MAX 1e7
 
-int main(void){
-    unsigned long long x;
-    while (1) {
-        scan(x);
-        printf("a:%u\n",x);
-        readInt(x);
-        printf("b:%u\n",x);
-    }
+template <typename T>
+long double calcPi(T limit) {
+	long double x = 1, y = 1, pi = 0;
+	for (long i = 0; i < limit; ++i) {
+		pi += y / x;
+		y = -y;
+		x += 2;
+	}
+	return 4.0L*pi;
+}
 
+int main(void) {
+	printf("%.25Lf\n", calcPi(MAX));
 }
